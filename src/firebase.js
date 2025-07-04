c888138bf43411ff268383d2234cdd4bc436ea2d
Amna Abrar <amna.abrar@gmail.com>
2025-07-04 13:09:06 +0500

    Add production fallback for Firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || "AIzaSyAwoKAVcj3AjC0YvOReVwCxgoKPd7PElko",
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || "task-manager-app-amna581.firebaseapp.com",
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || "task-manager-app-amna581",
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || "task-manager-app-amna581.firebasestorage.app",
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "605376689367",
+  appId: process.env.REACT_APP_FIREBASE_APP_ID || "1:605376689367:web:9b302b27c4c46b86762c92"
 };
 
 // Initialize Firebase
